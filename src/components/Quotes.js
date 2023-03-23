242afa69de86d24cd7695142e9a9fcce635521d4
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-03-23 23:03:00 +0100

    created random quote logic

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -4,19 +4,42 @@ export default function Quotes() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
 
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  function getQuote() {
+    let url =
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        let dataQuotes = data.quotes;
+        let randomNumber = Math.floor(Math.random() * dataQuotes.length);
+        let randomQuote = dataQuotes[randomNumber];
+
+        setQuote(randomQuote.quote);
+        setAuthor(randomQuote.author);
+      });
+  }
+
+  function handleClick() {
+    getQuote();
+  }
+
   return (
     <div id="quote-box">
       <div id="text">
-        <p></p>
+        <p>{quote}</p>
       </div>
       <div id="author">
-        <p></p>
+        <p>{author}</p>
       </div>
       <div id="buttons">
         <div id="tweet-quote">
           <a href="twitter.com/intent/tweet">Tweet link</a>
         </div>
-        <button id="new-quote"></button>
+        <button onClick={handleClick} id="new-quote"></button>
       </div>
     </div>
   );
