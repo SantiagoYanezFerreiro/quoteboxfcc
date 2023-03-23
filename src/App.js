e2156d19c18098231d04c42c0751042e3788daef
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2023-03-23 22:34:58 +0100

    Added  Quote component structure

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ import Quotes from "./components/Quotes";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Quotes />
-      </header>
+      <header className="App-header"></header>
+      <Quotes />
     </div>
   );
 }
--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,6 +1,25 @@
 import React, { useState, useEffect } from "react";
 
 export default function Quotes() {
-  const [quotes, setQuote] = useState("");
+  const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
+
+  return (
+    <div id="quote-box">
+      <div id="text">
+        <p></p>
+      </div>
+      <div id="author">
+        <p></p>
+      </div>
+      <div id="buttons">
+        <div id="tweet-quote">
+          <a href="twitter.com/intent/tweet">Tweet link</a>
+        </div>
+        <button id="new-quote"></button>
+      </div>
+    </div>
+  );
 }
+
+/*quote author props*/
